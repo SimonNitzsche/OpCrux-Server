8ce3e15a57e705f4dd6c4bc767b5b5749605697d
Simon <coolgametubeorginal@gmail.com>
2018-05-13 22:03:40 +0200

    Started on AMF3

---
+++ src/DataTypes/AMF3.hpp
@@ -0,0 +1,145 @@
+#pragma once
+#ifndef _AMF3_HPP
+#define _AMF3_HPP
+
+#include <string>
+#include <exception>
+#include <RakNet\BitStream.h>
+
+enum class AMF3_TYPE_MARKER : unsigned char {
+	undefined_marker = 0,
+	null_marker,
+	false_marker,
+	true_marker,
+	integer_marker,
+	double_marker,
+	string_marker,
+	xml_doc_marker,
+	date_marker,
+	array_marker,
+	object_marker,
+	xml_marker,
+	byte_array_marker,
+	vector_int_marker,
+	vector_uint_marker,
+	vector_double_marker,
+	vector_object_marker,
+	dictionary_marker
+};
+
+class U29 {
+private:
+	unsigned data : 29;
+public:
+	U29() {};
+	U29(int input) { data = (input >> 3); }
+	U29(RakNet::BitStream * input) {
+		data = 0;
+		for (int i = 0; i < 4; ++i) {
+			unsigned char buffer;
+			input->Read(buffer);
+			data += ((buffer & 0x7F)<<(8*i));
+
+			if (buffer & 0x80) { // first bit active
+				continue;
+			}
+
+		break;	
+		}
+	}
+
+	void write(RakNet::BitStream * output) {
+		unsigned long matcher = 0x7F;
+		for (int i = 0; i < 4; i++) {
+			bool needNewByteAfter = data & (matcher << (i + 1));
+			char buffer = (data & (matcher << i)) + (needNewByteAfter << 7);
+			output->Write(buffer);
+			if (!needNewByteAfter) return;
+		}
+	}
+
+	operator unsigned long() const {
+		return data;
+	}
+};
+
+class AMF3;
+
+class AMF3_TYPE {
+public:
+	AMF3_TYPE_MARKER get_type_marker() { throw new std::exception("Can't execute on interface."); }
+};
+
+class amf3_undefined : public AMF3_TYPE {
+public:
+	AMF3_TYPE_MARKER get_type_marker() { return AMF3_TYPE_MARKER::undefined_marker; }
+	amf3_undefined() {};
+};
+
+class amf3_null : public AMF3_TYPE {
+public:
+	AMF3_TYPE_MARKER get_type_marker() { return AMF3_TYPE_MARKER::null_marker; }
+};
+
+class amf3_boolean : public AMF3_TYPE {
+public:
+	AMF3_TYPE_MARKER get_type_marker() { return static_cast<AMF3_TYPE_MARKER>(static_cast<unsigned char>(AMF3_TYPE_MARKER::false_marker) + data); }
+private:
+	bool data;
+public:
+	amf3_boolean(bool input) : data(input) {}
+};
+
+class amf3_string : public AMF3_TYPE {
+public:
+	AMF3_TYPE_MARKER get_type_marker() { return AMF3_TYPE_MARKER::string_marker; }
+private:
+	std::string data;
+public:
+	amf3_string(std::string input) : data(input) {}
+	amf3_string(RakNet::BitStream * input) {
+
+	}
+};
+
+class amf3_array : public AMF3_TYPE {
+public:
+	AMF3_TYPE_MARKER get_type_marker() { return AMF3_TYPE_MARKER::array_marker; }
+private:
+public:
+	amf3_array(RakNet::BitStream * input) {
+
+	}
+};
+
+class AMF3 {
+public:
+	static AMF3_TYPE Read(RakNet::BitStream * bs) {
+		AMF3_TYPE_MARKER marker;
+		bs->Read(marker);
+		switch (marker) {
+		case AMF3_TYPE_MARKER::undefined_marker:
+			return amf3_undefined();
+		
+		case AMF3_TYPE_MARKER::null_marker:
+			return amf3_null();
+
+		case AMF3_TYPE_MARKER::false_marker:
+			return amf3_boolean(false);
+
+		case AMF3_TYPE_MARKER::true_marker:
+			return amf3_boolean(true);
+
+		case AMF3_TYPE_MARKER::string_marker:
+			return amf3_string(bs);
+		
+		case AMF3_TYPE_MARKER::array_marker:
+			return amf3_array(bs);
+
+		default:
+			throw new std::exception("Unknown marker.");
+		}
+	}
+};
+
+#endif
--- src/Main.cpp
+++ src/Main.cpp
@@ -14,7 +14,7 @@ std::vector<ILUServer *> virtualServerInstances;
 
 MasterServer *mS;
 BridgeMasterServer *masterServerBridge;
-
+#include "DataTypes\AMF3.hpp"
 int main(int argc, char* argv[]) {
 	enum class SERVERMODE { STANDALONE, MASTER, WORLD, AUTH } MODE_SERVER;
 	std::string ipMaster;
@@ -53,6 +53,18 @@ int main(int argc, char* argv[]) {
 
 	std::thread aT([]() { while (mS == nullptr || !mS->isDone) RakSleep(30); new AuthServer("127.0.0.1"); });
 	aT.detach();
+
+	// Test
+
+
+	//unsigned char * data = new unsigned char [16] { 0x09, 0x01, 0x0f, 0x76, 0x69, 0x73, 0x69, 0x62, 0x6c, 0x65, 0x02, 0x01, 0x0a, 0x00, 0x00, 0x00 };
+	
+	unsigned char * data = new unsigned char[4]{ 0xf7, 0xbf, 0xbf, 0xbf };
+
+	RakNet::BitStream *bs = new RakNet::BitStream(data, sizeof(data), false);
+	//AMF3::Read(bs);
+
+	U29 a = U29(bs);
 
 	while (true)RakSleep(30);
 
