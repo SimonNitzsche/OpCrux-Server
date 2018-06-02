77a8fe44627693c23071361a0c08d3e15491f2d9
Sinrai <colin.wallimann99@gmail.com>
2018-06-02 20:59:22 +0200

    Vector3 done

--- src/DataTypes/Vector3.hpp
+++ src/DataTypes/Vector3.hpp
@@ -41,11 +41,11 @@ public:
 
 	// Static methods
 	static float Dot(Vector3 lhs, Vector3 rhs) { return lhs.x*rhs.x + lhs.y*rhs.y + lhs.z*rhs.z; }
-	static Vector3 Cross(Vector3 lhs, Vector3 rhs) { return Vector3(abs(lhs.y*rhs.z-lhs.z*rhs.y), -abs(lhs.z*rhs.x-lhs.x*rhs.z), abs(lhs.x*rhs.y-lhs.y*rhs.x)); }
-	static float Angle(Vector3 from, Vector3 to) { return (float)(acos(Dot(from, to)/(from.magnitude()*to.magnitude()))*180/acos(-1)); }
+	static Vector3 Cross(Vector3 lhs, Vector3 rhs) { return Vector3(lhs.y*rhs.z-lhs.z*rhs.y, lhs.z*rhs.x-lhs.x*rhs.z, lhs.x*rhs.y-lhs.y*rhs.x); }
+	static float Angle(Vector3 from, Vector3 to) { return radiantodegree(acosf(Dot(from, to)/(from.magnitude()*to.magnitude()))); }
 	static Vector3 ClampMagnitude(Vector3 vector, float maxLength) { return vector.normalized()*maxLength; }
 	static float Distance(Vector3 a, Vector3 b) { return (a-b).magnitude(); }
-	static Vector3 Lerp(Vector3 a, Vector3 b, float t) { (t<1?t:t=1); return a+(b-a)*t; }
+	static Vector3 Lerp(Vector3 a, Vector3 b, float t) { clamp(t); return LerpUnclamped(a, b, t); }
 	static Vector3 LerpUnclamped(Vector3 a, Vector3 b, float t) { return a+(b-a)*t; }
 	static Vector3 Max(Vector3 lhs, Vector3 rhs) { return Vector3((lhs.x>rhs.x?lhs.x:rhs.x), (lhs.y>rhs.y?lhs.y:rhs.y), (lhs.z>rhs.z?lhs.z:rhs.z)); }
 	static Vector3 Min(Vector3 lhs, Vector3 rhs) { return Vector3((lhs.x<rhs.x?lhs.x:rhs.x), (lhs.y<rhs.y?lhs.y:rhs.y), (lhs.z<rhs.z?lhs.z:rhs.z)); }
@@ -56,16 +56,25 @@ public:
 	}
 	static Vector3 Normalize(Vector3 & value) {	value = value.normalized(); return value; } // Why return a Vector3?
 	static Vector3 RotateTowards(Vector3 current, Vector3 target, float maxRadiansDelta, float maxMagnitudeDelta) {
-		/// TODO implement
+		float angle = degreetoradian(Angle(current, target));
+		if (maxRadiansDelta > angle) { maxRadiansDelta = angle; }
+		else if (maxRadiansDelta < angle-M_PI) { maxRadiansDelta = (float)(angle-M_PI);}
+		Vector3 retVector = rotate(current, Cross(current, target), maxRadiansDelta);
+		float magnitudeDelta = (target.magnitude()-current.magnitude())*maxRadiansDelta/angle;
+		if (magnitudeDelta > maxMagnitudeDelta) { magnitudeDelta = maxMagnitudeDelta; }
+		float magnitude = retVector.magnitude();
+		return retVector*((magnitude+magnitudeDelta)/magnitude);
 	}
 	static Vector3 Scale(Vector3 a, Vector3 b) { return Vector3(a.x*b.x, a.y*b.y, a.z*b.z); }
 	static float SignedAngle(Vector3 from, Vector3 to, Vector3 axis) {
-		float angle = Angle(from, to);
-		if (Angle(Cross(from, to), axis) < 90) { angle *= -1; }
-		return angle;
+		Normalize(from); Normalize(to); Normalize(axis);
+		float angle = atan2(Dot(axis, Cross(from, to)), Dot(from, to));
+		return angle*180/(float)M_PI;
 	}
 	static Vector3 Slerp(Vector3 a, Vector3 b, float t) {
-		/// TODO implement
+		clamp(t);
+		Vector3 cross = Cross(a,b);
+		return rotate(a, cross, degreetoradian(Angle(a,b))*t).normalized()*(b.magnitude()*t+a.magnitude()*(1-t));
 	}
 
 	// Operators
@@ -97,7 +106,7 @@ public:
 	}
 	bool operator != (Vector3 other) const { return !(*this==other); }
 	bool operator != (float other) const { return !(*this==other); }
-	float operator [] (int index) const {
+	float& operator [] (int index) {
 		switch(index) {
 			case 0:	return this->x;
 			case 1:	return this->y;
@@ -105,6 +114,17 @@ public:
 			default: throw new std::runtime_error("Index out of bounds");
 		}
 	}
+private:
+	static Vector3 rotate(Vector3 vec, Vector3 axis, float angle) {
+		Normalize(axis);
+		return vec*cos(angle)+Cross(axis,vec)*sin(angle)+axis*Dot(axis,vec)*(1-cos(angle));
+	}
+	static void clamp(float &t) {
+		if (t < 0) { t=0; }
+		if (t > 1) { t=1; }
+	}
+	static float radiantodegree(float rad) { return rad*180/(float)M_PI; }
+	static float degreetoradian(float deg) { return deg*(float)M_PI/180; }
 };
 
 #endif // !_VECTOR3_HPP__
