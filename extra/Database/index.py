54ece89ffeaaf856498ee7156c71a41825359450
encry <17jbradford@tqacademy.co.uk>
2021-04-13 05:44:05 +0100

    working migration runner

--- extra/Database/index.py
+++ extra/Database/index.py
@@ -1,6 +1,8 @@
 import pyodbc
 import os
 from dataclasses import dataclass
+import configparser
+config = configparser.ConfigParser()
 
 @dataclass
 class Migration:
@@ -10,7 +12,15 @@ class Migration:
     ID: int
     Name: str
 
-for item in os.listdir("../patch/"):
+config.read("../../bin/conf/Database.ini")
+
+conn = pyodbc.connect('DRIVER={' + config["DBConnection"]["DBDRIVER"] +'};SERVER=' + config["DBConnection"]["DBHOST"] + ';UID=' + config["DBConnection"]["DBUSER"] + ';PWD=' + config["DBConnection"]["DBPASS"])
+cursor = conn.cursor()
+
+for item in os.listdir("."):
     itemList = item.split('_')
+    if not len(itemList) > 3 or not "sql" in item: continue
     localMigration: Migration = Migration(int(itemList[0]), int(itemList[1]), int(itemList[2]), int(itemList[3]), itemList[4].split('.')[0])
     print("Running migration: " + localMigration.Name)
+    file = open(item, "r")
+    cursor.execute(file.read())
